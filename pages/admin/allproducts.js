e1eea883721d49664381d0364a15683d8a50c313
Abhirup Kumar Bhowmick <94789673+abhirupkumar@users.noreply.github.com>
2022-08-16 21:08:12 +0530

    Updated

--- pages/admin/allproducts.js
+++ pages/admin/allproducts.js
@@ -21,7 +21,7 @@ const Allproducts = ({ products }) => {
         if (!myuser) {
             router.push('/')
         }
-        if(myuser && myuser.token && (myuser.email == 'abhirupkumar2003@gmail.com' || myuser.email == 'kabirlesoft@gmail.com')){
+        if(myuser && myuser.token && (myuser.email == 'abhirupkumar2003@gmail.com')){
             setAdmin(true)
         }
         else{
